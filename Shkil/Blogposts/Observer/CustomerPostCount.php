b4bb6258d775aa99a30066059beec5cda660b02a
greentoss <mrsoberman1@gmail.com>
2022-10-31 13:19:44 +0200

    changed observer

--- Shkil/Blogposts/Observer/CustomerPostCount.php
+++ Shkil/Blogposts/Observer/CustomerPostCount.php
@@ -4,6 +4,8 @@ namespace Shkil\Blogposts\Observer;
 
 use Exception;
 use Magento\Customer\Api\CustomerRepositoryInterface;
+use Magento\Customer\Model\ResourceModel\Customer as CustomerResource;
+use Magento\Customer\Model\CustomerFactory;
 use Magento\Customer\Model\Session;
 use Magento\Framework\Event\Observer as EventObserver;
 use Magento\Framework\Event\ObserverInterface;
@@ -13,20 +15,31 @@ class CustomerPostCount implements ObserverInterface
     /**
      * @var Session
      */
-    private Session $customer;
-
+    private $customer;
     /**
      * @var CustomerRepositoryInterface
      */
-    private CustomerRepositoryInterface $customerRepository;
+    private $customerRepository;
+    /**
+     * @var CustomerResource
+     */
+    private $customerResource;
+    /**
+     * @var CustomerFactory
+     */
+    private $customerFactory;
 
     public function __construct (
         Session $customer,
-        CustomerRepositoryInterface $customerRepository
+        CustomerRepositoryInterface $customerRepository,
+        CustomerResource $customerResource,
+        CustomerFactory $customerFactory,
     )
     {
         $this->customer = $customer;
         $this->customerRepository = $customerRepository;
+        $this->customerResource = $customerResource;
+        $this->customerFactory = $customerFactory;
     }
 
     /**
@@ -35,23 +48,25 @@ class CustomerPostCount implements ObserverInterface
      */
     public function execute(EventObserver $observer)
     {
-        $post = $observer->getEvent()->getData('post');
-
-        if (!$post) {
-            return $this;
-        }
+//        $post = $observer->getEvent()->getData('post');
+//        if (!$post) {
+//            return $this;
+//        }
         // if post doesn't exist, observer does nothing
+        $customerId = $this->customer->getCustomerId();
 
-//        $customerId = $this->customer->getCustomerId();
-//
-//        try {
+        try {
 //            $customer = $this->customerRepository->getById($customerId);
 //            $blogposts = (int) $customer->getCustomerAttribute('blogposts') + 1;
-//            $customer->setData('blogposts', $blogposts);
 //            $this->customerRepository->save($customer);
-//        } catch (Exception $e) {
-//            //TODO: Logger
-//        }
+            $customer = $this->customerFactory->create();
+            $this->customerResource->load($customer, $customerId);
+            $blogposts = (int) $customer->getData('blogposts') + 1;
+            $customer->setData('blogposts', $blogposts);
+            $this->customerResource->save($customer);
+        } catch (Exception $e) {
+            //TODO: Logger
+        }
         return $this;
     }
 }
